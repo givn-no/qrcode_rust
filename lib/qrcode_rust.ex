3e32045418a77a83e6a64b98a8d0960442cd4bbf
Håvard Lindset <havard@hoopla.no>
2021-11-17 12:52:41 +0100

    add generate_svg with some defaults

--- lib/qrcode_rust.ex
+++ lib/qrcode_rust.ex
@@ -3,4 +3,8 @@ defmodule QRCodeRust do
 
   def generate_svg(_data, _qr_kind, _ec_level, _fg_color, _bg_color),
     do: :erlang.nif_error(:nif_not_loaded)
+
+  def generate_svg(data) do
+    generate_svg(data, :square, :ec_l, "#222", "#fff")
+  end
 end
