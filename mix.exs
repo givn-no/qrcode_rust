1a6b00f1a24a46e321ec87fa46ac8a9c0654248b
Håvard Lindset <havard@givn.no>
2025-04-01 20:11:47 +0200

    config hex package

--- mix.exs
+++ mix.exs
@@ -11,7 +11,9 @@ defmodule QrcodeRust.MixProject do
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
-      package: package()
+      package: package(),
+      hex: hex(),
+      description: "QR code generator implemented as a rust nif"
     ]
   end
 
@@ -25,7 +27,8 @@ defmodule QrcodeRust.MixProject do
         "checksum-*.exs",
         "mix.exs"
       ],
-      links: %{"GitHub" => @source_url}
+      links: %{"GitHub" => @source_url},
+      licenses: ["MIT"]
     ]
   end
 
@@ -41,6 +44,13 @@ defmodule QrcodeRust.MixProject do
     [
       {:rustler_precompiled, "~> 0.6"},
       {:rustler, "~> 0.30.0", optional: true}
+    ]
+  end
+
+  defp hex do
+    [
+      api_url: System.get_env("HEX_API_URL"),
+      api_key: System.get_env("HEX_API_KEY")
     ]
   end
 end
